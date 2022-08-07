c43c1543212992f8313dcc3b075d26337401c2a8
Angela Kirby <angelakirby@email.arizona.edu>
2022-08-07 10:51:14 -0400

    Added R scirpt for data preprocessing

---
+++ DataPreprocessing.R
@@ -0,0 +1,14 @@
+# Perform any necessary data preprocessing.
+# Handle any missing data (impute or delete using your best judgment)
+
+# Analyze histograms of all continuous variables and normalize any non-normal features
+
+# Scale and/or smooth data if necessary
+
+# Discretize variables if necessary
+
+# Dummy code categorical variables if your dataset contains any
+
+# Identify outliers and handle them to your best judgment
+
+# Calculate any new variables if necessary with mutate()
