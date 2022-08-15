b98f689bf8c22917b76119aea8cf9031e48e5406
Angela Kirby <angelakirby@arizona.edu>
2022-08-15 09:12:23 -0400

    removed Method column

--- CleanedData.R
+++ CleanedData.R
@@ -82,5 +82,9 @@ dataBreachesDataFrame <- dataBreachesDataFrame %>%
            Method == "accidental leak" ~ 0,
            Method == "lost device" ~ 0))
 
+# remove Method column from the data frame
+dataBreachesDataFrame <- dataBreachesDataFrame %>%  
+  select(-Method)
+
 # convert data frame back into dataBreaches tibble
 dataBreaches <- as_tibble(dataBreachesDataFrame)
